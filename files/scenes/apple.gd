f2b5d3342055fa8723832b4698e90a809651bc50
Jakub Motyka <jakub.motyka2010@outlook.com>
2026-07-06 18:11:56 +0200

    Started making 1st minigame

---
+++ files/scenes/apple.gd
@@ -0,0 +1,14 @@
+extends Node2D
+@onready var player: CharacterBody2D = $"../player"
+@onready var self_area = $Area2D
+@onready var player_area = $"../player/Area2D"
+
+# make a signal
+signal apple_collected
+
+func _process(delta: float) -> void: # this runs EVERY FRAME! 
+	
+	if player_area.overlaps_area(self_area): # checks if overlapping
+		if self.visible:
+			emit_signal("apple_collected") #signal broadcast
+			self.hide() #removed from player sight; collected
